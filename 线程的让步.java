3084329c51ef6f7b449b88ef02665d354bda7bb5
ouuxxxi <1018743511@qq.com>
2020-03-25 13:27:33 +0800

    -

---
+++ 线程的让步.java
@@ -0,0 +1,17 @@
+package lesson2;
+
+public class ThreadYield {
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName());
+            }
+        }).start();
+        // 等待new Thread所有线程执行完毕，否则一直等待
+        while(Thread.activeCount() > 1){//使用调试的方式运行
+            Thread.yield();// 将当前线程由运行态--->就绪态
+        }
+        System.out.println(Thread.currentThread().getName());
+    }
+}
